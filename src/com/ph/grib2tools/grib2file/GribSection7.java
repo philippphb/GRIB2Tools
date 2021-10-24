819efcc9337b1b7f9b417859cc178b9d60005292
El Willibald <el.willi.bald@gmail.com>
2021-10-24 21:28:17 +0200

    added handling of sparse data grids using a Section 6 bit map to identify grid points with values

--- src/com/ph/grib2tools/grib2file/GribSection7.java
+++ src/com/ph/grib2tools/grib2file/GribSection7.java
@@ -19,6 +19,11 @@ public class GribSection7 extends GribSection {
 	private int numberDataPoints;
 	private DataRepresentationTemplate5x dataRepresentation;
 	public DataSection7x data;
+	
+	// Contains the full data grid. If a bit map is used, this field contains the
+	// full data grid, including the fields without a value, which are set to 0.
+	// If no bitmap is used, this field contains the section 7 data
+	public byte bitmapDecodedData[];
 
 	public GribSection7(int len, byte num, byte[] data) {
 		super(len, num, data);
@@ -40,19 +45,25 @@ public class GribSection7 extends GribSection {
 
 	public void readSection() {
 		ByteBuffer byteBuffer = ByteBuffer.wrap(sectiondata);
-		if (dataRepresentation.getClass().equals(DataRepresentationTemplate50.class)) { /* Nothing to do */ }
+		if (dataRepresentation.getClass().equals(DataRepresentationTemplate50.class)) { 
+			// No action required
+		}
 		else if (dataRepresentation.getClass().equals(DataRepresentationTemplate52.class)) {
 			data = new DataSection72(numberDataPoints, (DataRepresentationTemplate52) dataRepresentation, byteBuffer);
 		} else if (dataRepresentation.getClass().equals(DataRepresentationTemplate53.class)) {
 			data = new DataSection73(numberDataPoints, (DataRepresentationTemplate53) dataRepresentation, byteBuffer);
 		} else
 			System.out.println("Data Representation Template Number 7." + dataRepresentation + " not implemented.");
-	}
-
+	}	
+	
 	public void setDataRepresentation(int numberDataPoints,
                                       DataRepresentationTemplate5x dataRepresentation)
 	{
 		this.numberDataPoints = numberDataPoints;
 		this.dataRepresentation = dataRepresentation;
+	}
+	
+	public void setBitmapDecodedData(byte decodedData[]) {
+		bitmapDecodedData = decodedData;
 	}
 }
--- src/com/ph/grib2tools/grib2file/RandomAccessGribFile.java
+++ src/com/ph/grib2tools/grib2file/RandomAccessGribFile.java
@@ -43,6 +43,10 @@ public class RandomAccessGribFile extends GribFile {
 					section7[gridcnt] = (GribSection7)nextsection;
 					section7[gridcnt].setDataRepresentation(section5[gridcnt].numberDataPoints, section5[gridcnt].dataRepresentationTemplate);
 					section7[gridcnt].readData(gribfile);
+					
+					// Decode bit map to expand sparse data grid to full data grid
+					section7[gridcnt].setBitmapDecodedData(decodeDataBitmap(gridcnt));
+					
 					gridcnt++;
 				}
 				else {
@@ -54,7 +58,78 @@ public class RandomAccessGribFile extends GribFile {
 			finalizeImport(gribfile);
 		}		
 	}
+	
+	private byte[] decodeDataBitmap(int gridcnt) {
+		
+		//GribSection6 section6 = gribFile.getSection6(0);
+		if (section6[gridcnt].bitMapIndicator == 0) {
+			
+			GridDefinitionTemplate30 gridDefinition = (GridDefinitionTemplate30) this.getGridDefinitionTemplate();
+			DataRepresentationTemplate50 dataRepresentation = (DataRepresentationTemplate50) this.getDataRepresentationTemplate(gridcnt);		
+			int numbits = dataRepresentation.numberBits;
 
+			byte valuesArray[] = section7[gridcnt].sectiondata;
+			byte decodedGridData[] = new byte[gridDefinition.numberPointsLat*gridDefinition.numberPointsLon*2];
+			
+			if ((gridDefinition.scanningMode & 0x04) == 0x00) {
+				
+				// Holds the index of the next value in the value array of Section 7
+				int sourcevaluecnt = 0;
+				
+				for (int j=0; j<gridDefinition.numberPointsLat; j++) {
+					for (int i=0; i<gridDefinition.numberPointsLon; i++) {
+
+						// Index of the bit map
+						int bitmapIdx;
+						bitmapIdx = j*gridDefinition.numberPointsLon + i;
+
+						// Position of byte and bit of the bit map 
+						int bitmapBytePos = (int)Math.floor(bitmapIdx/8);
+						int bitmapBitPos = bitmapIdx % 8;
+						byte bitmapByte = section6[gridcnt].bitMap[bitmapBytePos];
+
+						short rawValue;
+						
+						// Grid point without value, here we set this to zero. Depending on the data, this is
+						// not the best value. Maybe value for empty grid pos should be made configurable
+						if ((bitmapByte & (1 << bitmapBitPos)) == 0) rawValue = 0;
+						//if ((bitmapByte & (1 << (8-bitmapBitPos))) == 0) rawValue = 0;
+						
+						// Grid point with value
+						else {
+							
+							// Position of the value in the array of values of Section 7
+							int sourcebitpos = sourcevaluecnt * numbits;
+
+							// Position of byte and bit of the value array 
+							int valstartbyte = (int)Math.floor(sourcebitpos/8);
+							int valstartbit = sourcebitpos % 8;
+
+							// Retrieve value from value array
+							ByteBuffer byteBuffer = ByteBuffer.wrap(valuesArray, valstartbyte, 2);
+							rawValue = byteBuffer.getShort();
+							rawValue = (short) (rawValue << valstartbit);
+							rawValue = (short) (rawValue >> (16-numbits));
+
+							// Increase the index of the next value in the value array of Section 7
+							sourcevaluecnt++;
+						}
+
+						// Write value in the ful grid data array
+						decodedGridData[bitmapIdx*2] = (byte) (rawValue >> 8);
+						decodedGridData[bitmapIdx*2+1] = (byte) (rawValue & 0xFF);
+					}
+				}
+			}	
+				
+			return decodedGridData;
+		}
+		
+		else {
+			return section7[gridcnt].sectiondata;
+		}
+	}
+	
 	// Extracts the data belonging to the passed coordinate (lat, lon) in degrees and returns the
 	// value represented by this data. If the passed coordinate is not a grid point of the
 	// data grid, the data belonging to the grid point closest to the passed position
@@ -84,31 +159,37 @@ public class RandomAccessGribFile extends GribFile {
 				GridDefinitionTemplate30 gridDefinition = (GridDefinitionTemplate30)section3.gridDefinitionTemplate;
 	
 				int deltaj = 0;
-
+/*
 				// Implementation of Scanning Modes
-				if ((gridDefinition.scanningMode & 0x01) == 0x01) log.warning("Scanning mode " + 0x01 + " not supported");
-				if ((gridDefinition.scanningMode & 0x02) == 0x02) log.warning("Scanning mode " + 0x02 + " not supported");
+//				if ((gridDefinition.scanningMode & 0x01) == 0x01) log.warning("Scanning mode " + 0x01 + " not supported");
+//				if ((gridDefinition.scanningMode & 0x02) == 0x02) log.warning("Scanning mode " + 0x02 + " not supported");
 				if ((gridDefinition.scanningMode & 0x04) == 0x04) log.warning("Scanning mode " + 0x04 + " not supported");
 				if ((gridDefinition.scanningMode & 0x08) == 0x08) log.warning("Scanning mode " + 0x08 + " not supported");
 				if ((gridDefinition.scanningMode & 0x10) == 0x10) log.warning("Scanning mode " + 0x10 + " not supported");
 				if ((gridDefinition.scanningMode & 0x20) == 0x20) log.warning("Scanning mode " + 0x20 + " not supported");
 				if ((gridDefinition.scanningMode & 0x40) == 0x40) deltaj = gridDefinition.jDirectionIncrement / 2;
 				if ((gridDefinition.scanningMode & 0x80) == 0x80) log.warning("Scanning mode " + 0x80 + " not supported");
+*/
+				deltaj = gridDefinition.getDeltaJ();
 	
 				// Calculate j index of the matrix containing the data the contains the data of the 
 				// passed latitude lat
 				int deltalat = lat - (gridDefinition.firstPointLat + deltaj);
-				int jidx = Math.round((float)deltalat / (float)gridDefinition.jDirectionIncrement);
+				//int jidx = Math.round((float)deltalat / (float)gridDefinition.jDirectionIncrement);
+//				int jidx = Math.round((float)deltalat / (float)gridDefinition.getStepJ());
+				int jidx = (int)Math.floor((float)deltalat / (float)gridDefinition.getStepJ());
 
 				// Calculate i index of the matrix containing the data the contains the data of the 
 				// passed longitude lon
 				int firstPointLon = gridDefinition.firstPointLon + 0;
 				if (firstPointLon >= GribFile.degToUnits(180)) firstPointLon -= GribFile.degToUnits(360);
 				int deltalon = lon - firstPointLon;
-				int iidx = Math.round((float)deltalon / (float)gridDefinition.iDirectionIncrement);
+				//int iidx = Math.round((float)deltalon / (float)gridDefinition.iDirectionIncrement);
+				int iidx = Math.round((float)deltalon / (float)gridDefinition.getStepI());
 
 				// Extract data belonging to the referred location and calculate the value represented by the data
-				byte data[] = sec7.sectiondata;
+				//byte data[] = sec7.sectiondata;
+				byte data[] = sec7.bitmapDecodedData;
 				short unsignedraw = ByteBuffer.wrap(data).getShort((jidx*gridDefinition.numberPointsLon+iidx)*bytesperval);
 				val = sec5.calcValue(unsignedraw); 
 			}
@@ -154,23 +235,25 @@ public class RandomAccessGribFile extends GribFile {
 				GridDefinitionTemplate30 gridDefinition = (GridDefinitionTemplate30)section3.gridDefinitionTemplate;
 
 				int deltaj = 0;
-
+/*
 				// Implementation of Scanning Modes
-				if ((gridDefinition.scanningMode & 0x01) == 0x01) log.warning("Scanning mode " + 0x01 + " not supported");
-				if ((gridDefinition.scanningMode & 0x02) == 0x02) log.warning("Scanning mode " + 0x02 + " not supported");
+//				if ((gridDefinition.scanningMode & 0x01) == 0x01) log.warning("Scanning mode " + 0x01 + " not supported");
+//				if ((gridDefinition.scanningMode & 0x02) == 0x02) log.warning("Scanning mode " + 0x02 + " not supported");
 				if ((gridDefinition.scanningMode & 0x04) == 0x04) log.warning("Scanning mode " + 0x04 + " not supported");
 				if ((gridDefinition.scanningMode & 0x08) == 0x08) log.warning("Scanning mode " + 0x08 + " not supported");
 				if ((gridDefinition.scanningMode & 0x10) == 0x10) log.warning("Scanning mode " + 0x10 + " not supported");
 				if ((gridDefinition.scanningMode & 0x20) == 0x20) log.warning("Scanning mode " + 0x20 + " not supported");
 				if ((gridDefinition.scanningMode & 0x40) == 0x40) deltaj = gridDefinition.jDirectionIncrement / 2;
 				if ((gridDefinition.scanningMode & 0x80) == 0x80) log.warning("Scanning mode " + 0x80 + " not supported");					
-
+*/
+				deltaj = gridDefinition.getDeltaJ();
 deltaj = 0;				
 				// Find j indices of the grid points of the matrix containing the data that surround the
 				// passed latitude lat
 				int deltalat = lat - (gridDefinition.firstPointLat + deltaj);
 				//int jidx1 = Math.round((float)deltalat / (float)gridDefinition.jDirectionIncrement);
-				int jidx1 = (int)Math.floor((float)deltalat / (float)gridDefinition.jDirectionIncrement);
+				//int jidx1 = (int)Math.floor((float)deltalat / (float)gridDefinition.jDirectionIncrement);
+				int jidx1 = (int)Math.floor((float)deltalat / (float)gridDefinition.getStepJ());
 				
 				// Correct indices when end of the array dimension is reached
 				if (jidx1 >= gridDefinition.numberPointsLat-1) jidx1--; 
@@ -179,17 +262,20 @@ deltaj = 0;
 				// Find i indices of the grid points of the matrix containing the data that surround the
 				// passed longitude lon
 				int firstPointLon = gridDefinition.firstPointLon + 0;
-				if (firstPointLon >= GribFile.degToUnits(180)) firstPointLon -= GribFile.degToUnits(360);
+// 17.10.21
+//				if (firstPointLon >= GribFile.degToUnits(180)) firstPointLon -= GribFile.degToUnits(360);
 				int deltalon = lon - firstPointLon;
 				//int iidx1 = Math.round((float)deltalon / (float)gridDefinition.iDirectionIncrement);
-				int iidx1 = (int)Math.floor((float)deltalon / (float)gridDefinition.iDirectionIncrement);
+				//int iidx1 = (int)Math.floor((float)deltalon / (float)gridDefinition.iDirectionIncrement);
+				int iidx1 = (int)Math.floor((float)deltalon / (float)gridDefinition.getStepI());
 
 				// Correct indices when end of the array dimension is reached
 				if (iidx1 >= gridDefinition.numberPointsLon-1) iidx1--; 
 				int iidx2 = iidx1 + 1;
 
 				
-				byte data[] = sec7.sectiondata;				
+				//byte data[] = sec7.sectiondata;				
+				byte data[] = sec7.bitmapDecodedData;				
 
 				// Extract data of the four grid points surrounding the passed coordinate and calculate the
 				// values represented by the data
@@ -202,13 +288,15 @@ deltaj = 0;
 				
 				// Find latitudes of the grid points of the matrix containing the data that surround the
 				// passed latitude lat
-				int lat1 = (gridDefinition.firstPointLat + deltaj) + jidx1 * gridDefinition.jDirectionIncrement;
-				int lat2 = lat1 + gridDefinition.jDirectionIncrement;
+				//int lat1 = (gridDefinition.firstPointLat + deltaj) + jidx1 * gridDefinition.jDirectionIncrement;
+				//int lat2 = lat1 + gridDefinition.jDirectionIncrement;
+				int lat1 = (gridDefinition.firstPointLat + deltaj) + jidx1 * gridDefinition.getStepJ();
+				int lat2 = lat1 + gridDefinition.getStepJ();
 
 				// Find longitudes of the grid points of the matrix containing the data that surround the
 				// passed longitude lon
-				int lon1 = firstPointLon + iidx1 * gridDefinition.iDirectionIncrement;
-				int lon2 = lon1 + gridDefinition.iDirectionIncrement;
+				int lon1 = firstPointLon + iidx1 * gridDefinition.getStepI();
+				int lon2 = lon1 + gridDefinition.getStepI();
 
 				// Interpolate value belonging to the passed location
 				float val1x = val11 + (val12-val11) * (lon-lon1) / (lon2-lon1);				
